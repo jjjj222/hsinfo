00cf6524744eae75621fe3ad4aacbc9abdd5a2f8
jjjj222 <jjjj222.tso@gmail.com>
2015-09-19 16:11:14 -0500

    hello world!

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>608 - project 1</title>
+        <link rel="shortcut icon" href="pic/cacophony.jpg">
+        <link type="text/css" rel="stylesheet" href="css/default.css">
+    </head>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+
+</body>
+</html>
